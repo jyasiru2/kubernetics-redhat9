8bb6b15bcf1ecc01c486a2451dad9a92745dc41e
jyasiru2 <161309923+jyasiru2@users.noreply.github.com>
2024-05-07 11:54:27 +0530

    pom.xml changes

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -48,9 +48,7 @@ public class NumericApplicationTests {
 
     @Test
     public void welcomeMessage() throws Exception {
-        this.mockMvc
-                .perform(get("/"))
-                .andDo(print())
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Kubernetes DevSecOps"));
     }
 
